0b63070e65c6c22b1588101bc0116202fa906d9f
Kyra Bankhead <bankheak@oregonstate.edu>
2022-10-27 11:49:15 -0700

    Create relative paths for data

--- code/template_code.R
+++ code/template_code.R
@@ -11,6 +11,7 @@ require(rmarkdown)  # “Knit” button (Ctrl+Shift+K) displays preview
 require(ggplot2)  # Plot designs
 
 
+
 # PART 2: applying NBDA to 'begging' data ---------------------------------
 
 ## load all necessary packages
@@ -22,22 +23,22 @@ require(NBDA)
 ## Read in all network data and turn into matrix
 
 # Vertical network
-vert.data <- read.csv("social_vertical.csv", row.names=1, header=TRUE)
+vert.data <- read.csv("../data/social_vertical.csv", row.names=1, header=TRUE)
 vert.data <- as.matrix(vert.data)
 
 # Horizontal network
-horz.data <- read.csv("social_horizontal.csv", row.names=1, header=TRUE)
+horz.data <- read.csv("../data/social_horizontal.csv", row.names=1, header=TRUE)
 horz.data <- as.matrix(horz.data)
 
 # Ecological network
-ecol.data <- read.csv("HR_overlaps.csv", row.names=1, header=TRUE)
+ecol.data <- read.csv("../data/HR_overlaps.csv", row.names=1, header=TRUE)
 ecol.data <- as.matrix(ecol.data)
 
 # Relatedness network
-relate.data <- read.csv("relatedness.csv", row.names=1, header=TRUE)
+relate.data <- read.csv("../data/relatedness.csv", row.names=1, header=TRUE)
 relate.data <- as.matrix(relate.data)
 
 # read ILVs
-ILV.data <- read.csv("ILVs.csv", header=TRUE, sep=",")
+ILV.data <- read.csv("../data/ILVs.csv", header=TRUE, sep=",")
 ILV.data[ILV.data=="<NA>"]=NA
 
