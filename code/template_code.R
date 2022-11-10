a16da781e0aef38f3ddca01f3c2d4483ecffafe4
Kyra Bankhead <bankheak@oregonstate.edu>
2022-11-10 09:46:53 -0800

    Reduce networks by cut-off point

--- code/template_code.R
+++ code/template_code.R
@@ -3,7 +3,7 @@
 # Set working directory here
 setwd("C:/Users/bankh/My_Repos/Dolphins")
 
-
+###########################################################################
 # PART 1: calculate dyadic home range overlaps ----------------------------
 
 ## load all necessary packages
@@ -11,7 +11,7 @@ require(rmarkdown)  # “Knit” button (Ctrl+Shift+K) displays preview
 require(ggplot2)  # Plot designs
 
 
-
+###########################################################################
 # PART 2: applying NBDA to 'begging' data ---------------------------------
 
 ## load all necessary packages
@@ -21,24 +21,54 @@ install_github("whoppitt/NBDA")
 require(NBDA)
 
 ## Read in all network data and turn into matrix
+### Vertical network
+vert_all <- read.csv("social_vertical.csv", row.names=1, header=TRUE)
+vert_all <- as.matrix(vert_all)
 
-# Vertical network
-vert.data <- read.csv("../data/social_vertical.csv", row.names=1, header=TRUE)
-vert.data <- as.matrix(vert.data)
+### Horizontal network
+hor_all <- read.csv("social_horizontal.csv", row.names=1, header=TRUE)
+hor_all <- as.matrix(hor_all)
 
-# Horizontal network
-horz.data <- read.csv("../data/social_horizontal.csv", row.names=1, header=TRUE)
-horz.data <- as.matrix(horz.data)
+### Ecological network
+ecol_all <- read.csv("HR_overlaps.csv", row.names=1, header=TRUE)
+ecol_all <- as.matrix(ecol_all)
 
-# Ecological network
-ecol.data <- read.csv("../data/HR_overlaps.csv", row.names=1, header=TRUE)
-ecol.data <- as.matrix(ecol.data)
+### Relatedness network
+relate_all <- read.csv("relatedness.csv", row.names=1, header=TRUE)
+relate_all <- as.matrix(relate_all)
 
-# Relatedness network
-relate.data <- read.csv("../data/relatedness.csv", row.names=1, header=TRUE)
-relate.data <- as.matrix(relate.data)
+### ILVs
+ILV_all <- read.csv("ILVs.csv", header=TRUE, sep=",")
+ILV_all[ILV_all=="<NA>"]=NA
 
-# read ILVs
-ILV.data <- read.csv("../data/ILVs.csv", header=TRUE, sep=",")
-ILV.data[ILV.data=="<NA>"]=NA
+## Make list of IDs that have been seen at least 7 times
+ILV <- subset(ILV_all, subset=ILV_all$Number_sightings>6) # at least 7 sightings
+ILV <- subset(ILV, subset=ILV$Not_weaned==0) # separate weaned
 
+IDs <- ILV$id_individual
+length(IDs) #___ individuals remain
+
+## Reduce vertical social network to only include the 415 individuals 
+## that made the cut-off point
+num <- which(colnames(vert_all) %in% IDs)
+vert_all <- vert_all[num, num]
+dim(vert_all)
+class(vert_all)
+
+## Repeat for horizontal social network
+num <- which(colnames(hor_all) %in% IDs)
+hor_all <- hor_all[num, num]
+dim(hor_all)
+class(hor_all)
+
+## Repeat for ecological network
+num <- which(colnames(ecol_all) %in% IDs)
+ecol <- ecol_all[num, num]
+dim(ecol)
+class(ecol)
+
+## Repeat for genetic network
+num <- which(colnames(relate_all) %in% IDs)
+relate <- relate_all[num, num]
+dim(relate)
+class(relate)
