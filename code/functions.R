081a9f0159ca3a50e37b07fdf970d8204d5a295d
Kyra Bankhead <bankheak@oregonstate.edu>
2022-11-10 11:47:16 -0800

    Start making the SRI function

--- code/functions.R
+++ code/functions.R
@@ -8,7 +8,18 @@
 setwd("C:/Users/bankh/OneDrive/Documents/Homework/OSU/R Homework")
 
 # SIMPLE-RATIO INDEX ------------------------------------------------------
-SRI<- function(x){
-  
+SRI<- function(df){
+  z<-ncol(df) # Name number of columns
+  var.names = names(df)  # Make a placeholder for variable names
+  sum.table = c() # Make a place for summary table
+    x<- 
+    yA<-
+    yB<-
+    yAB<- 
+  SRIAB<- x/x+yA+yB+yAB
+  for(i in 1:z) {
+    sum.table<-
+        rbind(sum.table,c(mean(x[,i]),sd(x[,i]),min(x[,i]),max(x[,i])))
+  }
 }
 
