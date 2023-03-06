07bab8e04ce1d9af4919ec45aff9634049b9381f
Kyra Bankhead <bankheak@oregonstate.edu>
2023-03-06 15:28:48 -0800

    Update modularity analysis

--- code/network_structure.R
+++ code/network_structure.R
@@ -18,6 +18,7 @@ require(statnet)
 
 # Read in social association matrix
 nxn<- read.csv("nxn.csv")
+nxn<- as.matrix(nxn)
 
 ## Create social network
 ig <- graph_from_adjacency_matrix(as.matrix(nxn),
@@ -44,7 +45,7 @@ plot(ig,
 )
 
 ###########################################################################
-# PART 2: Network Properties ------------------------------------------------
+# PART 2: Network & Global Properties ------------------------------------------------
 
 # Source edgelist function
 source("../code/functions.R")
@@ -52,5 +53,77 @@ source("../code/functions.R")
 # Edgelist: the first two are the nodes' labels; the third is the edge (or link) weights
 el <- matrix_to_edgelist(nxn, rawdata = FALSE, idnodes = FALSE)
 
-# Weighted Shortest path lengths for all individuals
+# Centrality measures
+## Betweenness centrality
+betweenness_w(el, alpha=1)       
+
+# Closeness centrality
+closeness_w(el, alpha=1)
+
+# Degree and strength centrality
+degree_w(el, measure=c("degree","output"), type="out", alpha=1)
+
+# Connectance or Density: proportion of realized links
+edge_density(ig, loops = FALSE)
+
+#' Breakdown: connectance = length(which(as.dist(orca_hwi)!=0))/(N*(N-1)/2)
+#' Number of nodes (number of rows in the association matrix)
+N = nrow(nxn)
+#' Number of possible links: 
+#' Nodes*(Nodes-1)/2: (-1 removes the node itself; /2 removes repetitions)
+total = N*(N-1)/2
+# Number of realized links: all non-zero cells in the association matrix
+real = length(which(as.dist(nxn)!=0))
+# Connectance: realized/total
+real/total
+
+# Shortest path lengths (geodesics) and diameter: binary and weighted
+# all binary shortest path lengths between nodes
+distances(ig)
+
+# mean shortest path
+mean_distance(ig)
+
+# Binary shortest path lengths
+distance_table(ig, directed=FALSE)
+
+# All Weighted Shortest path lenghts (or geodesics)
 distance_w(el)
+# weighted diameter
+max(as.dist(distance_w(el)))
+
+# Clustering Coefficient
+# Weighted clustering coefficients (using 5 different methods). 
+clustering_w (el, measure=c("am", "gm", "mi", "ma", "bi"))
+
+
+###########################################################################
+# PART 3: Modularity ------------------------------------------------
+
+# Create a network from the first two columns
+dolp_ig <- graph.edgelist(el[,1:2])
+# Add the edge weights to this network by assigning an edge attribute called 'weight'.
+E(dolp_ig)$weight <- as.numeric(el[,3])
+# Create undirect network
+dolp_ig <- as.undirected(dolp_ig)
+
+# Plot
+plot(dolp_ig, edge.width=E(dolp_ig)$weight*4, vertex.size=10, vertex.label=NA, edge.curved=F)
+
+# Algorythm
+newman <- cluster_leading_eigen(dolp_ig, steps = -1, weights = E(dolp_ig)$weight, 
+                                start = NULL, options = arpack_defaults, callback = NULL, 
+                                extra = NULL, env = parent.frame())
+plot(dolp_ig)
+
+# Random color scheme
+col <- rgb(runif(10), runif(10), runif(10))
+
+# Assign a random color to individuals of each module ('community')
+newman$membership
+
+for (i in 1:length(newman$membership)){
+  sample(col)
+  V(dolp_ig)$color[which(newman$membership==i)] = col[i]
+}
+plot(dolp_ig)
