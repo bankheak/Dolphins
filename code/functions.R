75ed2948d4e28b07035e210a8f1d1b48f808bbf6
Kyra Bankhead <bankheak@oregonstate.edu>
2023-07-18 14:45:39 -0700

    Calculate HRO

--- code/functions.R
+++ code/functions.R
@@ -13,6 +13,8 @@ library(doParallel)
 library(tcltk)
 library(survival)
 library(combinat)
+library(rgeos)
+library(raster)
 
 
 # SIMPLE-RATIO INDEX ------------------------------------------------------
@@ -20,7 +22,7 @@ library(combinat)
 #' @param matr A binary matrix depicting individuals in the columns and groups in the rows
 #' @return A square matrix in which each cell is an estimate of a dyadic social relationship, from 0 (never seen in the same group) to 1 (always seen in the same group)
 
-SRI.func<-  function (matr) {
+SRI.func <-  function (matr) {
   if (any(is.na(matr))) {
     matr <- na.omit(matr)
     cat("The data matrix contains NA, and have been removed.\n")
@@ -46,7 +48,7 @@ SRI.func<-  function (matr) {
 }
 
 
-# Null Permutations -------------------------------------------------------
+# NULL PERMUTATIONS -------------------------------------------------------
 #' @description shuffles binary matrices under different restrictions. 
 #' @param mat A quantitative matrix
 #' @param iter Number of random matrices to be created 
@@ -114,6 +116,55 @@ matrix_to_edgelist=function(mat, rawdata, idnodes){
     nodes = data.frame(colnames(mat), ID)
     names(nodes)=c("tnet code", "real ID")
     print(nodes)}
+}
+
+
+# HRO ------------------------------------------------------
+#' @description This function calculateS home range overlap using the formula: 
+#' HRO = (Rij/Ri) * (Rij/Rj), where Rij is the overlap in range between individuals i and j, 
+#' Ri is the range of individual i, and Rj is the range of individual j. 
+#' @param kernel.poly home range polygons for each individual in a SpatialPolygonsDataFrame
+#' @return A square matrix in which each cell is an estimate of dyadic homerange overlap
+
+HRO <- function(dolph.kernel.poly) {
+  
+  # Empty matrix to store HRO values for each pair of individuals
+  n_individuals <- length(dolph.kernel.poly@data$id)
+  HRO_matrix <- matrix(NA, nrow = n_individuals, ncol = n_individuals, 
+                       dimnames = list(dolph.kernel.poly@data$id, dolph.kernel.poly@data$id))
+  
+  # Loop through each pair of individuals to calculate HRO
+  for (i in 1:n_individuals) {
+    for (j in (i+1):n_individuals) {
+      # Get the home range polygons for individuals i and j
+      poly_i <- dolph.kernel.poly@polygons[[i]]
+      poly_j <- dolph.kernel.poly@polygons[[j]]
+      
+      # Calculate the intersection area between the home ranges (Rij)
+      intersection_area <- function(poly1, poly2) {
+        if (is.null(gIntersects(poly1, poly2))) {
+          return(0)  # No intersection, return 0
+        } else {
+          intersect_poly <- gIntersection(poly1, poly2)
+          return(gArea(intersect_poly))
+        }
+      }
+      
+      intersect_area <- intersection_area(poly_i, poly_j)
+      
+      # Calculate the home range area for individuals i and j (Ri and Rj)
+      home_range_area_i <- st_area(poly_i)
+      home_range_area_j <- st_area(poly_j)
+      
+      # Calculate HRO using the formula: HRO = (Rij/Ri) * (Rij/Rj)
+      HRO_value <- (intersect_area / home_range_area_i) * (intersect_area / home_range_area_j)
+      
+      # Store the HRO value in the matrix
+      HRO_matrix[dolph.kernel.poly@data$id[i], dolph.kernel.poly@data$id[j]] <- HRO_value
+      HRO_matrix[dolph.kernel.poly@data$id[j], dolph.kernel.poly@data$id[i]] <- HRO_value
+    }
+  }
+  
 }
 
 
--- code/homerange_overlaps.R
+++ code/homerange_overlaps.R
@@ -20,6 +20,7 @@ library(viridis) # Color pallette
 library(gridExtra) # grid.arrange function
 library(ggplot2)
 library(adehabitatHR) # Kernel density 
+library(rgdal) # Overlap
 
 # Read in file
 sample_data <- read.csv("sample_data.csv")
@@ -28,18 +29,17 @@ sample_data <- read.csv("sample_data.csv")
 mybasemap <- ggmap(get_stamenmap(bbox = c(left = -83, bottom = 27, right = -82, top = 28)))
 
 # Extract coordinates
-coord_data <- cbind(sample_data[,c('Date', 'StartLat', 'StartLon', 'Code')]) # Subset Date and Coordinates #
+coord_data <- cbind(sample_data[,c('Date', 'StartLat', 'StartLon', 'Code', 'subYear')]) # Subset Date and Coordinates #
 ## Format date and year
 coord_data$Date <- as.Date(as.character(coord_data$Date), format="%Y-%m-%d")
-coord_data$Year <- as.numeric(format(coord_data$Date, format = "%Y"))
 ## Give descriptive names
-colnames(coord_data) <- c("date", "y", "x", "id", "year")
+colnames(coord_data) <- c("date", "y", "x", "id", "subyear")
 
 # Seperate map per years
-years <- unique(coord_data$year)
+years <- unique(coord_data$subyear)
 coord_years <- list()
 for (i in 1:length(years)) {
-  coord_years[[i]] <- subset(coord_data, subset=c(coord_data$year == years[i]))
+  coord_years[[i]] <- subset(coord_data, subset=c(coord_data$subyear == years[i]))
 }    
 
 # Test one year at a time
@@ -47,18 +47,19 @@ coord_data <- coord_years[[1]]
 
 # Eliminate IDs with less than 5 locations
 coord_data <- subset(coord_data, subset=c(coord_data$id != "None"))
+coord_data <- coord_data[!is.na(coord_data$x) & !is.na(coord_data$y),]
+
 ID <- unique(coord_data$id)
 obs_vect <- NULL
 for (i in 1:length(ID)) {
   obs_vect[i]<- sum(coord_data$id == ID[i])
 }
 sub <- data.frame(ID, obs_vect)
-sub <- subset(sub, subset=c(sub$obs_vect > 4))
+sub <- subset(sub, subset=c(sub$obs_vect > 10))
 coord_data <- subset(coord_data, coord_data$id %in% c(sub$ID))
 
 # Only include three columns (id, x, and y coordinates) for making MCP's
 dolph.sp <- coord_data[, c("id", "y", "x")] 
-dolph.sp <- dolph.sp[!is.na(dolph.sp$x) & !is.na(dolph.sp$y),]
 
 # Create a simple feature data frame (sf)
 coord_data_sf <- st_as_sf(dolph.sp, coords = c("x", "y"), crs = 4326)
@@ -76,15 +77,62 @@ coordinates(dolph.sp) <- c("x", "y")
 # Set the initial CRS for data to WGS84 (latitude and longitude)
 proj4string(dolph.sp) <- CRS( "+proj=utm +zone=17 +datum=WGS84 +units=m +no_defs" )
 
+# Create kernel estimates for each id
+kernel.lscv <- kernelUD(dolph.sp, h = "LSCV")  # LSCV = least squares cross validation
+
+# Get the names or IDs of the individuals that are of concern
+selected_individuals <- c("F101", "F191", "F192", "FB15", "FB35", "FB41", "FB93")
+
+# Repeat code above to calculate appropriate bandwidth for IDs of concern
+concern.sp <- coord_data[, c("id", "y", "x")] 
+concern.sp <- subset(concern.sp, id %in% selected_individuals) # Only include selected ids
+
+concern_sf <- st_as_sf(concern.sp, coords = c("x", "y"), crs = 4326)
+concern.sf <- st_transform(concern_sf, crs = paste0("+proj=utm +zone=17 +datum=WGS84 +units=m +no_defs"))
+concern.sp$x <- st_coordinates(concern.sf)[, 1]
+concern.sp$y <- st_coordinates(concern.sf)[, 2]
+concern.sp <- concern.sp[!is.na(concern.sp$x) & !is.na(concern.sp$y),]
+
+coordinates(concern.sp) <- c("x", "y")
+
+proj4string(concern.sp) <- CRS( "+proj=utm +zone=17 +datum=WGS84 +units=m +no_defs" )
+
+# Now recalculate kernel estimates for each id
+kernel.con <- kernelUD(concern.sp, h = "LSCV")
+plotLSCV(kernel.con) # it looks like a bandwidth of 1000 will be good enough
+
+kernel.lscv <- kernelUD(dolph.sp, h = 1000)
+
+# 95% of estimated distribution
+dolph.kernel.poly <- getverticeshr(kernel.lscv, percent = 95)  
+print(dolph.kernel.poly)  # returns the area of each polygon
+
+###########################################################################
+# PART 2: Plot HRO ------------------------------------------------------------
+
 # Calculate MCPs for each dolphin
 dolph.mcp <- mcp(dolph.sp, percent = 95)
 
-# Plot homeranges
-plot(dolph.sp, col = as.factor(dolph.sp@data$id), pch = 16, asp = 1)
-plot(dolph.mcp, col = alpha(1:5, 0.5), add = TRUE)
+# Convert dolph.sp and dolph.mcp to sf objects
+dolph.sp_sf <- st_as_sf(dolph.sp)
+dolph.mcp_sf <- st_as_sf(dolph.mcp)
 
-# Create kernel estimates for each id
-kernel.ref <- kernelUD(dolph.sp, h = "href", extent = 0.5)  # href = the referen
+# Convert ggmap basemap to raster
+mybasemap_raster <- as.raster(mybasemap)
 
-dolph.kernel.poly <- getverticeshr(kernel.ref, percent = 95)  
-print(turtle.kernel.poly)  # returns the area of each polygon
+# Plot the ggmap basemap as a raster and overlay the two sf objects
+ggplot() +
+  annotation_raster(mybasemap_raster, xmin = -83, xmax = -82, ymin = 27, ymax = 28) +
+  geom_sf(data = dolph.mcp_sf, aes(fill = as.factor(id)), alpha = 0.5) +
+  geom_sf(data = dolph.sp_sf, aes(color = factor(id)), size = 3, shape = 16) +
+  scale_fill_viridis_d(option = "D", begin = 0.2, end = 0.8, direction = -1) +  # Use viridis color palette
+  scale_color_manual(values = rainbow(length(unique(dolph.sp_sf$id)))) +
+  theme_minimal() +
+  labs(fill = "MCP", color = "Dolphin number") +
+  coord_sf() + theme(legend.position = "none")  # Remove the legend
+
+###########################################################################
+# PART 2: Calculate Dyadic HRO Matrix: HRO = (Rij/Ri) * (Rij/Rj)------------------------------------------------------------
+
+# Get HRO 
+kov <- kerneloverlaphr(kernel.lscv, method="HR", lev=95)
